4c9b266abf75c8b0778dcf7a8242a3ed3137a5a5
GFM97 <33311109+GFM97@users.noreply.github.com>
2019-03-01 15:46:11 +0100

    Shooting -1
    - Player Shooting.
    - Collide with obstacles and they disappear. The obstacles will be powerups.

---
+++ Shooting/Assets/Scripts/Spawn_S/Pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	public Spawner movement;
+
+	void OnTriggerEnter(Collider target)
+	{
+		if (target.tag == "Red")
+		{
+			movement.enabled = false;
+		}
+
+		if (target.gameObject.layer == LayerMask.NameToLayer("Pickups"))
+		{
+			Destroy(target.gameObject);
+		}
+	}
+}
+
