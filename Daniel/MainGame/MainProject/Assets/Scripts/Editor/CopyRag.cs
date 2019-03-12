e047eac0da18557d2fc00772b1be655b0914de69
danzaq <34459944+danzaq@users.noreply.github.com>
2019-03-12 12:59:00 +0100

    created main project

---
+++ Daniel/MainGame/MainProject/Assets/Scripts/Editor/CopyRag.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CopyRag : EditorWindow
+{
+    GameObject source;
+    GameObject target;
+
+    [MenuItem("Window/CopyComponents")]
+
+    public static void ShowWindow()
+    {
+        GetWindow<CopyRag>("CopyComponents");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("I suck", EditorStyles.boldLabel);
+
+        source = EditorGUILayout.ObjectField(new GUIContent("Source Object"), source, typeof(GameObject),  true) as GameObject;
+        target = EditorGUILayout.ObjectField(new GUIContent("Target Object"), target, typeof(GameObject), true) as GameObject;
+
+        
+
+        if (GUILayout.Button("Copy"))
+        {
+            CopySpecialComponents(source,target);
+        }
+        
+    }
+
+
+    private void CopySpecialComponents(GameObject _sourceGO, GameObject _targetGO)
+    {
+        foreach (var component in _sourceGO.GetComponents<Component>())
+        {
+            var componentType = component.GetType();
+            Debug.Log(componentType);
+
+            if (componentType != typeof(Transform) &&
+                componentType != typeof(MeshFilter) &&
+                componentType != typeof(MeshRenderer) &&
+                componentType != typeof(BoxCollider) &&
+                componentType != typeof(SphereCollider) &&
+                componentType != typeof(CapsuleCollider)
+                )
+            {
+                Debug.Log("Found a component of type " + component.GetType());
+                UnityEditorInternal.ComponentUtility.CopyComponent(component);
+                UnityEditorInternal.ComponentUtility.PasteComponentAsNew(_targetGO);
+                Debug.Log("Copied " + component.GetType() + " from " + _sourceGO.name + " to " + _targetGO.name);
+            }
+        }
+    }
+}
---
+++ Daniel/MainGame/MainProject/Assets/Scripts/Window/CopyComponents.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class CopyComponents : EditorWindow
+{
+
+    Component[] components;
+
+    public GameObject SourceGameObject;
+    public GameObject TargetGameObjec;
+    static Type CompType;
+
+    [MenuItem("Window/Copy")]
+    public static void ShowWindow()
+    {
+        GetWindow<CopyComponents>("Copy");
+    }
+
+    void OnGUI()
+    {
+        //Window Code
+        GUILayout.Label("Copy Components.", EditorStyles.boldLabel);
+
+        if(GUILayout.Button("Copy"))
+        {
+            Copy();
+        }
+    }
+
+    void Colorize()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                //renderer.sharedMaterial.color = color;
+            }
+        }
+    }
+    
+    void Copy()
+    {
+        //if (SourceGameObject == null) SourceGameObject = gameObject;
+
+        components = SourceGameObject.GetComponents<Component>();
+        for (int i = 0; i < components.Length; ++i)
+        {
+            if (components[i] != null && components[i] != this)
+            {
+
+
+                CompType = components[i].GetType();
+                TargetGameObjec.AddComponent(CompType);
+
+                //print(CompType);
+            }
+
+            if (components[i] == null)
+                Debug.LogWarning("Warning, component n° " + i + " not found in this project");
+        }
+    }
+}
