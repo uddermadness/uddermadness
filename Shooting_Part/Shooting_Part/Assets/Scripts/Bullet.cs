489d190b36edd7c733dd8afe3706ddc4770eee3f
GFM97 <33311109+GFM97@users.noreply.github.com>
2019-03-26 12:22:52 +0100

    Shooting_1
    - Shoot Bullets
    - Pick Up Ammo
    - Reticle with mouse

---
+++ Shooting_Part/Shooting_Part/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour {
+
+	Rigidbody rb;
+
+	public float force = 50f;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb.AddForce(transform.TransformDirection(Vector3.forward) * force, ForceMode.Impulse);
+		Destroy(gameObject, 0.5f);
+	}
+}
---
+++ Shooting_Part/Shooting_Part/Assets/Scripts/Shooting.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine; 
+using System.Collections;
+ 
+ public class Shooting : MonoBehaviour 
+ {
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+    
+    public int maxAmmo = 10;
+    private int currentAmmo = -1;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+     
+    public Transform bulletSpawner;
+
+    void Start ()
+    {
+        if (currentAmmo == -1)
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable ()
+    {
+        isReloading = false;  
+    }
+     
+    void Fire()
+    {
+        currentAmmo--;
+        
+        Rigidbody bulletClone = Instantiate<Rigidbody>(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+    }
+
+    void Update () 
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+
+    public void AddBullets(string name)
+    {
+        Debug.Log("Picked up " + name);
+    }
+
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+ }
