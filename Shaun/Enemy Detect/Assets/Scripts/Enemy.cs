ebe260bfa6c0c7ae09c486a4609d6dc89cfc0583
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2019-04-14 10:34:36 +0200

    now with a stalker following the player

---
+++ Shaun/Enemy Detect/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    NavMeshAgent pathfinder;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent> ();
+        target = GameObject.FindGameObjectWithTag ("Player").transform;
+    
+        StartCoroutine (UpdatePath ());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator UpdatePath() {
+        float refreshRate = 0.5f;
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,0,target.position.z);
+            pathfinder.SetDestination (target.position);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
---
+++ Shaun/Uddermadness3rd/Main/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	public bool lockCursor;
+	public float mouseSensitivity = 0.125f;
+	public Transform target;
+	public float dstFromTarget = 2;
+	public Vector2 pitchMinMax = new Vector2 (-40, 50);
+	public Vector3 offset;
+
+	public float rotationSmoothTime = .12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	void Start() {
+		// this is to lock and make the cursor(mouse) invisible
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void LateUpdate () {
+		// the sensitivity of the mouse
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+		// problem make the camer rotate the characeter
+
+		transform.position = target.position - transform.forward * dstFromTarget;
+
+		// this is to ofset the camera from the caracter
+		// Vector3 desiredPosition = target.position + offset;
+        // Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, mouseSensitivity);
+        // transform.position = smoothedPosition;
+
+	}
+
+}
