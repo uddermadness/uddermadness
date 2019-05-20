e5ef9d9fe51174ca8d0559d5c4134ef3d9aaa1a0
GFM97 <33311109+GFM97@users.noreply.github.com>
2019-05-20 13:31:31 +0200

    Shooting - Final
    - Camera over the shoulder
    - Shooting in the middle

---
+++ Shooting_Part/Uddermadness3rd/Main/Assets/Editor/CharacterCameraEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ThirdPersonCamera))]
+public class CharacterCameraEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Place Camera"))
+        {
+            (target as ThirdPersonCamera).Snap();
+        }
+    }
+}
---
+++ Shooting_Part/Uddermadness3rd/Main/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	public bool lockCursor;
+	public float mouseSensitivity = 10;
+	public Transform target;
+	public Vector3 cameraOffset = new Vector3(0.5f, 2f, 2.5f);
+	public float dstFromTarget = 2;
+	public float offsetTarget = 2;
+	public Vector2 pitchMinMax = new Vector2 (-40, 85);
+
+	public float rotationSmoothTime = .12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	void Start() {
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void LateUpdate () {
+		Snap();
+	}
+
+	public void Snap()
+	{
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+		transform.position = target.position - transform.forward * cameraOffset.z + transform.right * cameraOffset.x + transform.up * cameraOffset.y;
+	}
+
+}
