df37ee70e044bf434e70d1e86d66f2b18c4bc97e
yannku <34515343+yannku@users.noreply.github.com>
2019-05-29 03:20:31 +0200

    door

---
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    public DoorController doorController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Activate();
+        }
+    }
+      
+
+    public void Activate()
+    {
+        if (doorController != null)
+        {
+            doorController.SetOpen(true);
+        }
+    }
+}
