653493e51442bbe200af3f2be8af132b0f420c97
namazaci <34474282+namazaci@users.noreply.github.com>
2019-06-01 01:22:49 +0200

    Finished the sidebar

--- Prototype/Assets/Scripts/TabControl.cs
+++ Prototype/Assets/Scripts/TabControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
+/* [System.Serializable]
 public class TabItem
 {
     public Image buttonGraphic, iconGraphic;
@@ -12,13 +12,13 @@ public class TabItem
 
     public GameObject view;
 } 
+ */
 
-[ExecuteInEditMode]
 public class TabControl : MonoBehaviour
 {
-    public TabItem[] items;
+    /* public TabItem[] items;
 
-    public Color primaryColor, secondaryColor;
+    public buttonImage primaryImage, secondaryImage;
 
     private void OnEnable()
     {
@@ -28,9 +28,9 @@ public class TabControl : MonoBehaviour
     public void SwitchTabIndex(int index)
     {
         ClearAllButtons();
-        items[index].buttonGraphic.color = secondaryColor;
-        items[index].iconGraphic.color = primaryColor;
-        items[index].buttonText.color = primaryColor;
+        items[index].buttonGraphic.image = secondaryImage;
+        items[index].iconGraphic.image = primaryImage;
+        items[index].buttonText.image = primaryImage;
         items[index].view.SetActive(true);
     }
 
@@ -38,10 +38,41 @@ public class TabControl : MonoBehaviour
     {
         foreach (TabItem item in items)
         {
-            item.buttonGraphic.color = primaryColor;
-            item.iconGraphic.color = secondaryColor;
-            item.buttonText.color = secondaryColor;
+            item.buttonGraphic.image = primaryImage;
+            item.iconGraphic.image = secondaryImage;
+            item.buttonText.image = secondaryImage;
             item.view.SetActive(false);
+        }
+    } */
+
+    public Sprite OnClick; // Drag your first sprite here
+    public Sprite NotClicked; // Drag your second sprite here
+
+    private SpriteRenderer spriteRenderer; 
+
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+        if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+        spriteRenderer.sprite = OnClick; // set the sprite to sprite1
+    }
+    void Update()
+    {
+        //Press space to change the Sprite of the Image
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeSprite();
+        }
+    }
+
+    void ChangeSprite() {
+        if (spriteRenderer.sprite == OnClick && spriteRenderer == null) // if the spriteRenderer sprite = sprite1 then change to sprite2
+        {
+            spriteRenderer.sprite = NotClicked;
+        }
+         else
+        {
+            spriteRenderer.sprite = OnClick; // otherwise change it back to sprite1
         }
     }
 }
