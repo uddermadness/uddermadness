80f929039a7034727a6871eac00d61731719be15
danzaq <34459944+danzaq@users.noreply.github.com>
2019-05-31 14:02:00 +0200

    grinder prefab created
    item spin script adjust to offer adjustable axis to make it more modular

---
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform movingPlatforms;
+
+    public Transform position1;
+
+    public Transform position2;
+
+    private Vector3 targetPosition;
+
+    public bool disableOnStart = false;
+
+    public float speed;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = position1.position;
+        ChangeTarget();
+        if (disableOnStart) Deactivate();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movingPlatforms.position = Vector3.MoveTowards(movingPlatforms.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(targetPosition, movingPlatforms.position) <= 0.1f)
+        {
+            ChangeTarget();
+        }
+    }
+
+    public void Activate()
+    {
+        enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        enabled = false;
+    }
+
+    void ChangeTarget()
+    {
+        if (targetPosition.Equals(position1.position))
+        {
+            targetPosition = position2.position;
+        }
+        else
+        {
+            targetPosition = position1.position;
+        }
+        
+    }     
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 1f, 1f, 0.5f);
+
+        if (position1 != null)
+        {
+            Gizmos.DrawWireCube(position1.position, Vector3.one * 1f);
+        }
+
+        if (position2 != null)
+        {
+            Gizmos.DrawWireCube(position2.position, Vector3.one * 1f);
+        }
+
+        if (position1 != null && position2 != null)
+        {
+            Gizmos.DrawLine(position1.position, position2.position);
+        }
+    }
+}
