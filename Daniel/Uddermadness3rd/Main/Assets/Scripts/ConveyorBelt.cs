eea3c1a4a756079c70790b5845b12d1d01bbe27e
yannku <34515343+yannku@users.noreply.github.com>
2019-06-01 02:47:17 +0200

    Conveyor

--- Daniel/Uddermadness3rd/Main/Assets/Scripts/ConveyorBelt.cs
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/ConveyorBelt.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
+    public Transform endPoint;
+    public GameObject belt;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endPoint.position, speed * Time.deltaTime);
     }
 }
