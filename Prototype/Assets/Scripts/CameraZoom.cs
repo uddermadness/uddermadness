ea09966f02bd04470be2d05bb2320366d072e8ce
namazaci <34474282+namazaci@users.noreply.github.com>
2019-06-03 01:00:38 +0200

    trying to make the map work
    Trying to add capsule and a plane to make it work in the map section but its not working
    
    Trying to make the zoom in and out with the buttons

--- Prototype/Assets/Scripts/CameraZoom.cs
+++ Prototype/Assets/Scripts/CameraZoom.cs
@@ -10,8 +10,29 @@ public class CameraZoom : MonoBehaviour {
 	float minZoom = 1f;
 
 	float curZoom = 0.42f;
+
+	public Button m_PlusButton, m_MinusButton;
+
+	void Start()
+	{
+		m_PlusButton = gameObject.GetComponent<Button>();
+		m_MinusButton = gameObject.GetComponent<Button>();
+
+	}
+
+	public void ZoomIn()
+	{
+		curZoom = Mathf.Clamp(curZoom + 1f * Time.deltaTime, maxZoom, minZoom);
+	}
+
+	public void ZoomOut()
+	{
+		curZoom = Mathf.Clamp(curZoom - 1f * Time.deltaTime, maxZoom, minZoom);
+	}
+	
+
 	void Update(){
-		if (Input.GetKey(KeyCode.UpArrow))
+		/* if (Input.GetKey(KeyCode.UpArrow))
 		{
 			curZoom = Mathf.Clamp(curZoom + 1f * Time.deltaTime, maxZoom, minZoom);
 		}
@@ -19,7 +40,7 @@ public class CameraZoom : MonoBehaviour {
 		if (Input.GetKey(KeyCode.DownArrow))
 		{
 			curZoom = Mathf.Clamp(curZoom - 1f * Time.deltaTime, maxZoom, minZoom);
-		}
+		} */
 
 		m_OrthographicCamera.orthographicSize = curZoom;
 	} 
