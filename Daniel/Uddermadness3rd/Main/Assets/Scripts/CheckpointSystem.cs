aafd5c367e1f4e786a6e1c87e83b706154cee65a
danzaq <34459944+danzaq@users.noreply.github.com>
2019-06-03 12:35:25 +0200

    checkpoint system

---
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/CheckpointSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointSystem : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 checkpointPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Player.transform.position = checkpointPosition;
+        }
+    }
+        void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            checkpointPosition = Player.transform.position;
+            Debug.Log(checkpointPosition);
+        }   
+    }
+}
