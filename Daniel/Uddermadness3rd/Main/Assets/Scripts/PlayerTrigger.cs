c415dc459c01ee28691afaba0fc2802218cb0065
danzaq <34459944+danzaq@users.noreply.github.com>
2019-06-07 23:09:20 +0200

    prefab tagging and collider setups

---
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerTrigger : MonoBehaviour
+{
+
+    public CheckpointSystem checkpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            gameObject.transform.position = checkpoint.checkpointPosition;
+        }
+        if (other.tag == "Done")
+        {
+             SceneManager.LoadScene(0);
+        }
+    }
+}
