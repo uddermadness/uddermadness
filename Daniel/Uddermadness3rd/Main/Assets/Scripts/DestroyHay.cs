64668da4f7942d44fc01b71d0b3776d2074647fb
yannku <34515343+yannku@users.noreply.github.com>
2019-06-07 16:18:07 +0200

    Added Checkpoints and some assets
    Added Checkpoints
    Hay Destroys using fire bullet
    Added small part of level to get the pepper
    Added some assets in first room

---
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/DestroyHay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHay : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pepper")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
