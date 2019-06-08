8049e120cf74b7765ae8b75f7e6eceba560ac36a
danzaq <34459944+danzaq@users.noreply.github.com>
2019-06-08 13:55:37 +0200

    adjust hierarchy

--- Daniel/Uddermadness3rd/Main/Assets/Scripts/Bullet.cs
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/Bullet.cs
@@ -10,14 +10,16 @@ public class Bullet : MonoBehaviour {
 	public float force = 50f;
 
 	private void Awake() {
+		//finding rigid component
 		rb = GetComponent<Rigidbody>();
 	}
 
 	// Use this for initialization
 	void Start () {
+		//shooting direction 
 		Vector3 shootDir = Random.insideUnitSphere * 0.1f;
 		shootDir.z = 1f;
-
+		//adding force to the bullet and destorying it after 2 seconds
 		rb.AddForce(transform.TransformDirection(shootDir) * force, ForceMode.Impulse);
 		Destroy(gameObject, 2f);
 	}
