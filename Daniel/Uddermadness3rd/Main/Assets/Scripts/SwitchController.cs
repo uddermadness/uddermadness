013acf3c5325a477ce62d60748e62f4bbb9e1b12
yannku <34515343+yannku@users.noreply.github.com>
2019-06-08 16:05:37 +0200

    added another comment

--- Daniel/Uddermadness3rd/Main/Assets/Scripts/SwitchController.cs
+++ Daniel/Uddermadness3rd/Main/Assets/Scripts/SwitchController.cs
@@ -8,6 +8,7 @@ public class SwitchController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //if switch is jit by bullet with a tag of milk the boolean is set to true
         if (other.tag == "Milk")
         {
             Activate();
@@ -17,6 +18,7 @@ public class SwitchController : MonoBehaviour
 
     public void Activate()
     {
+        //setting boolean to true
         if (doorController != null)
         {
             doorController.SetOpen(true);
